29f994a49e4557544bedd2c587a817f1a86b5f1a
farinaema <117036663+farinaema@users.noreply.github.com>
2022-12-02 15:14:41 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+include <iostream>
+using namespacce std;
 
+bool primo ( int a ){
+
+    int i=1, n=a;
+
+    if (a != 1){
+        do{
+            i++;
+            n=a%i;
+        } while ( n );
+    } else {
+        return true;
+    }
+
+    if ( i==a ){
+        return true;
+    } else {
+        return false;
+    }
+}
